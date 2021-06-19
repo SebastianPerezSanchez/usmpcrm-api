3b9f1ad9540a4e631f3446ee13a5255bc4acf054
Diego Perez <diego.fid@hotmail.com>
2021-06-19 03:09:06 -0500

    talvez

--- src/main/java/crmapi/usmp/usmpcrmapi/repository/CustomerRepository.java
+++ src/main/java/crmapi/usmp/usmpcrmapi/repository/CustomerRepository.java
@@ -22,6 +22,6 @@ public interface CustomerRepository extends JpaRepository<Customer,Integer>{
     @Query("select count(*) as customers from Customer c where extract(month from c.registerDate) = extract(month from c.registerDate)")
     List<Map<String, Object>> queryCustomersthisMonth();
 
-    @Query("select sum(case when extract(year from age(tc.birthdate)) between 18 and 30 then 1 else 0 end) as a, sum(case when extract(year from age(tc.birthdate)) between 31 and 45 then 1 else 0 end) as b, sum(case when extract(year from age(tc.birthdate)) > 46 then 1 else 0 end) as c from Customer tc")
+    @Query("select concat('ejemplo') as ejemplo, sum(case when extract(year from age(tc.birthdate)) between 18 and 30 then 1 else 0 end) as a, sum(case when extract(year from age(tc.birthdate)) between 31 and 45 then 1 else 0 end) as b, sum(case when extract(year from age(tc.birthdate)) > 46 then 1 else 0 end) as c from Customer tc")
     List<Map<String, Object>> queryCustomersAgeRange();
 }
